787f4502139081b14b96cfe085826a42628f9299
George Matar <gm185617@ohio.edu>
2020-05-14 18:01:19 -0400

    lab 1 update 3

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: George Matar
+ *        Date: 5/14/2020
+ * Description: lab 1
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
